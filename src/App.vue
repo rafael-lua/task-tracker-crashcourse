2c2d82b0743e9b01c7a76e70fd3ce75e549f04bb
Rafael <rafael_oliveira@gmx.com>
2021-03-03 19:26:22 -0300

    Added header component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
-  <h1>Hello world!</h1>
+  <div class="container">
+    <Header />
+  </div>
 </template>
 
 <script>
+import Header from "./components/Header"
+
 export default {
   name: 'App',
   components: {
-    
+    Header
   }
 }
 </script>
