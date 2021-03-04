655e2fa9289ba8b10521f918f51b927f20e85d96
Rafael <rafael_oliveira@gmx.com>
2021-03-04 00:39:06 -0300

    Added tasks

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,50 @@
 <template>
   <div class="container">
     <Header title="Task Tracker" />
+    <Tasks :tasks="tasks" />
+    <!-- 
+      To understand better: https://vuejs.org/v2/guide/syntax.html
+      Basically, mustache {{}} sintaxe can't be used with html attributes, so you use binds instead.
+     -->
   </div>
 </template>
 
 <script>
 import Header from "./components/Header"
+import Tasks from "./components/Tasks"
 
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    Tasks
+  },
+  data() {
+    return {
+      tasks: []
+    }
+  },
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: "Doctors Appointment",
+        day: "March 1st at 2:30pm",
+        remainder: true,
+      },
+      {
+        id: 2,
+        text: "Meeting at School",
+        day: "March 3rd at 1:30pm",
+        remainder: true,
+      },
+      {
+        id: 3,
+        text: "Food Shopping",
+        day: "March 3rd at 11:00am",
+        remainder: false,
+      }
+    ];
   }
 }
 </script>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,15 @@
+<template>
+  <div v-for="task in tasks" :key="task.id">
+    <h3>{{ task.text }}</h3>
+  </div>
+  <!-- "When using v-for, vue expects a unique key for each iteration" - Explains the :key bind -->
+</template>
+
+<script>
+export default {
+  name: "Tasks",
+  props: {
+    tasks: Array
+  }
+}
+</script>
