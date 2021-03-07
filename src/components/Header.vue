8a797be87093afaef0f695215654beb831cf401c
Rafael <rafael_oliveira@gmx.com>
2021-03-06 21:02:36 -0300

    Added computed property on header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <header>
     <h1>{{ title }}</h1>
-    <Button @btn-click="$emit('toggle-add-task')" :text="styleAddTask == true ? 'Close' : 'Add Task'" :color="styleAddTask == true ? 'red' : 'green'" />
+    <Button v-show="homePage" @btn-click="$emit('toggle-add-task')" :text="styleAddTask == true ? 'Close' : 'Add Task'" :color="styleAddTask == true ? 'red' : 'green'" />
   </header>
 </template>
 
@@ -17,7 +17,16 @@ export default {
   components: {
     Button
   },
-  emits: ["toggle-add-task"]
+  emits: ["toggle-add-task"],
+  computed: {
+    homePage() {
+      if(this.$route.path === "/") {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
 }
 </script>
 
